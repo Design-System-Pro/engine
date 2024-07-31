11f69e6fa0ea2e41693179274ed70b536d2282c5
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-07-31 17:41:13 +0200

    add migrate db github actions

--- apps/dashboard/src/lib/database/migrations/0004_flimsy_lilandra.sql
+++ apps/dashboard/src/lib/database/migrations/0004_flimsy_lilandra.sql
@@ -20,21 +20,3 @@ $$;
 create or replace trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_auth_user();
-
-
--- -- inserts a row into public.design_systems
--- create or replace function public.handle_new_account()
--- returns trigger
--- language plpgsql
--- security definer set search_path = public
--- as $$
--- begin
---   insert into public.design_systems default values returning id as design_system_id;
---   update public.accounts set design_system_id = design_system_id where user_id = new.id;
--- end;
--- $$;
-
--- -- trigger the function every time an auth user is created in auth.users
--- create or replace trigger on_account_created
---   after insert on public.accounts
---   for each row execute function public.handle_new_account();
