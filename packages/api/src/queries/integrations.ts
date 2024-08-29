742c23a1cedb5bf65c8f1008493f1d938f69b1b0
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-08-29 13:15:53 +0200

    prevent integrations from showing in not setup accounts

--- packages/api/src/queries/integrations.ts
+++ packages/api/src/queries/integrations.ts
@@ -1,31 +1,44 @@
 import { eq } from '@ds-project/database';
 import type { DSContext } from '../types/context';
+import { AccountsToProjects, Integrations } from '@ds-project/database/schema';
 
 export const selectGithubIntegration = async ({ ctx }: { ctx: DSContext }) => {
-  const queryResult = await ctx.database.query.Integrations.findFirst({
-    columns: {
-      data: true,
-    },
-    where: (integrations) => eq(integrations.type, 'github'),
-    with: {
-      project: {
-        columns: {
-          id: true,
-        },
-        with: {
-          accountsToProjects: {
-            columns: {
-              projectId: true,
-            },
-            where: (accountsToProjects) =>
-              eq(accountsToProjects.accountId, ctx.account.id),
-          },
-        },
-      },
-    },
-  });
+  const [queryResult] = await ctx.database
+    .select({
+      data: Integrations.data,
+    })
+    .from(Integrations)
+    .leftJoin(
+      AccountsToProjects,
+      eq(AccountsToProjects.projectId, Integrations.projectId)
+    )
+    .where(eq(AccountsToProjects.accountId, ctx.account.id))
+    .limit(1);
 
   if (queryResult?.data?.type === 'github') {
+    return {
+      ...queryResult,
+      data: queryResult.data,
+    };
+  }
+
+  return undefined;
+};
+
+export const selectFigmaIntegration = async ({ ctx }: { ctx: DSContext }) => {
+  const [queryResult] = await ctx.database
+    .select({
+      data: Integrations.data,
+    })
+    .from(Integrations)
+    .leftJoin(
+      AccountsToProjects,
+      eq(AccountsToProjects.projectId, Integrations.projectId)
+    )
+    .where(eq(AccountsToProjects.accountId, ctx.account.id))
+    .limit(1);
+
+  if (queryResult?.data?.type === 'figma') {
     return {
       ...queryResult,
       data: queryResult.data,
