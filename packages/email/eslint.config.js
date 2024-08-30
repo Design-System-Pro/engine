c13e13e4eac68091d7823a6dcbfcb46ffd58e664
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-08-30 18:11:18 +0200

    add email package for email templating

---
+++ packages/email/eslint.config.js
@@ -0,0 +1,11 @@
+import baseConfig from '@ds-project/eslint/base';
+import reactConfig from '@ds-project/eslint/react';
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+  {
+    ignores: ['dist/**'],
+  },
+  ...baseConfig,
+  ...reactConfig,
+];
