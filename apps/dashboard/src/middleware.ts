03b010fb3007735aafe6ad3e819c4766d02ac17f
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-09-11 11:57:18 +0200

    ignore monitoring from middleware

--- apps/dashboard/src/middleware.ts
+++ apps/dashboard/src/middleware.ts
@@ -60,6 +60,6 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|api/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|api/|monitoring/|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 };
