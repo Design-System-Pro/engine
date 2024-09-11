d1941ff020ed524f791c6ccc213f52b89b6cd25d
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-09-11 13:26:35 +0200

    move sentry to _proxy and exclude it from middleware

--- apps/dashboard/sentry.server.config.ts
+++ apps/dashboard/sentry.server.config.ts
@@ -2,10 +2,11 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
+import { clientEnv } from '@/env/client';
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  enabled: process.env.NODE_ENV === 'production',
+  enabled: clientEnv.NEXT_PUBLIC_VERCEL_ENV === 'production',
   dsn: 'https://bee796b3d7d7f0364e1dc326183331f0@o4507860870299648.ingest.de.sentry.io/4507860871872592',
 
   // Adjust this value in production, or use tracesSampler for greater control
