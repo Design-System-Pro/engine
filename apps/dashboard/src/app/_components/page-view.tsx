13696f8f001e00f180ca0d5cfa68592e7206e04a
Tomás Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-09-16 11:26:08 +0200

    add posthog page view (#34)

---
+++ apps/dashboard/src/app/_components/page-view.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
+import { usePostHog } from 'posthog-js/react';
+
+export function PageView(): null {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+
+  useEffect(() => {
+    // Track pageviews
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture('$pageview', {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams, posthog]);
+
+  return null;
+}
--- apps/dashboard/src/lib/analytics/provider.tsx
+++ apps/dashboard/src/lib/analytics/provider.tsx
@@ -9,9 +9,13 @@ if (
   clientEnv.NEXT_PUBLIC_VERCEL_ENV === 'production'
 ) {
   posthog.init(clientEnv.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: '/_proxy/posthog/ingest',
+    api_host: '/_proxy/posthog',
     ui_host: 'https://eu.posthog.com',
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+    loaded: (posthog) => {
+      if (clientEnv.NEXT_PUBLIC_VERCEL_ENV === 'development')
+        posthog.debug(true);
+    },
   });
 }
 
