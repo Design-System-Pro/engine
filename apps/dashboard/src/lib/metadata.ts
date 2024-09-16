482cdc0e04b9e0c205d1ae77acb90c71b19bb7a0
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-09-16 15:22:19 +0200

    update metadata og

--- apps/dashboard/src/lib/metadata.ts
+++ apps/dashboard/src/lib/metadata.ts
@@ -11,6 +11,8 @@ export function getMetadata({ title }: GetMetadataArgs = {}): Metadata {
     description: 'Design System DevOps Engine',
     keywords: 'design system, devops, engine, design, system, design tokens',
     openGraph: {
+      siteName: 'DS Pro',
+      url: `https://${clientEnv.NEXT_PUBLIC_VERCEL_URL}/api/og`,
       images: [`https://${clientEnv.NEXT_PUBLIC_VERCEL_URL}/api/og`],
     },
   };
