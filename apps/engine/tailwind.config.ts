781e1db0dcab5eaabaefa7c334b240363a081d34
Tomás Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-09-24 11:21:08 +0200

    rename dashboard app to engine (#42)

--- apps/engine/tailwind.config.ts
+++ apps/engine/tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  corePlugins: {
+    preflight: false, // Usage of packages/components includes preflight already
+  },
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
