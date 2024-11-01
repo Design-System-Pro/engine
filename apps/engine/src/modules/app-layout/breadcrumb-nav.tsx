83652b598a073fd0e2c3ecb01e9c2f552c310cf7
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-11-01 19:49:14 +0100

    feat(ui): add faq and groups for sources and destinations

--- apps/engine/src/modules/app-layout/breadcrumb-nav.tsx
+++ apps/engine/src/modules/app-layout/breadcrumb-nav.tsx
@@ -25,7 +25,7 @@ export function BreadcrumbNav() {
     <Breadcrumb>
       <BreadcrumbList>
         <BreadcrumbItem className="hidden capitalize md:block">
-          Connections
+          {connectionGroup}
         </BreadcrumbItem>
         <BreadcrumbSeparator className="hidden md:block" />
         <BreadcrumbItem>
