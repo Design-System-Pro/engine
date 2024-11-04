576962fb550b1dbb70c02a4833d9fe2f6b95b14c
Tomas Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-11-04 10:18:20 +0100

    feat(ui): improve github integration card with clear UX

--- apps/engine/src/components/integration-settings/integration-settings.tsx
+++ apps/engine/src/components/integration-settings/integration-settings.tsx
@@ -16,7 +16,6 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-  Switch,
 } from '@ds-project/components';
 import type { ReactNode } from 'react';
 
@@ -41,61 +40,62 @@ export function IntegrationSettings({
   isOpen,
   onOpenChange,
 }: IntegrationSettingsProps) {
-  const shouldIgnoreAlert = !isEnabled;
   return (
     <>
-      <Sheet open={isOpen} onOpenChange={onOpenChange}>
-        <SheetTrigger asChild>
-          <Button variant="outline" className="space-x-2" disabled={!isEnabled}>
-            <Icons.GearIcon />
-            <span>Settings</span>
-          </Button>
-        </SheetTrigger>
-        <SheetContent className="flex flex-col">
-          <SheetHeader>
-            <div className="flex items-center gap-2">
-              {integrationLogo}
-              <SheetTitle>
-                <h3>Integration settings</h3>
-              </SheetTitle>
-            </div>
-            <SheetTitle>{name}</SheetTitle>
-            <SheetDescription>Configure integration</SheetDescription>
-          </SheetHeader>
-          {children}
-        </SheetContent>
-      </Sheet>
-      <AlertDialog open={shouldIgnoreAlert ? false : undefined}>
-        <AlertDialogTrigger asChild>
-          <span>
-            <Switch
-              checked={isEnabled}
-              onCheckedChange={shouldIgnoreAlert ? onEnabledChange : undefined}
-            />
-          </span>
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
-            <AlertDialogDescription>
-              We will disconnect the integration with {name}. You may need to
-              reconfigure settings if you decide to reconnect this integration
-              later.
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction
-              variant="destructive"
-              onClick={() => {
-                onEnabledChange(false);
-              }}
-            >
-              Disconnect
-            </AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
+      {isEnabled ? (
+        <>
+          <Sheet open={isOpen} onOpenChange={onOpenChange}>
+            <SheetTrigger asChild>
+              <Button variant="outline" className="space-x-2">
+                <Icons.GearIcon />
+                <span>Settings</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent className="flex flex-col">
+              <SheetHeader>
+                <div className="flex items-center gap-2">
+                  {integrationLogo}
+                  <SheetTitle>
+                    <h3>Integration settings</h3>
+                  </SheetTitle>
+                </div>
+                <SheetTitle>{name}</SheetTitle>
+                <SheetDescription>Configure integration</SheetDescription>
+              </SheetHeader>
+              {children}
+            </SheetContent>
+          </Sheet>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button variant="destructive">Disconnect</Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                <AlertDialogDescription>
+                  We will disconnect the integration with {name}. You may need
+                  to reconfigure settings if you decide to reconnect this
+                  integration later.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  variant="destructive"
+                  onClick={() => {
+                    onEnabledChange(false);
+                  }}
+                >
+                  Disconnect
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </>
+      ) : (
+        <Button onClick={() => onEnabledChange(true)}>Connect</Button>
+      )}
     </>
   );
 }
