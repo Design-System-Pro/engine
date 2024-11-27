625ec3d84ea31cc8110ae6f7ac8f67aba3cd305e
Tomás Francisco <4301103+tomasfrancisco@users.noreply.github.com>
2024-11-27 14:04:46 +0100

    fix: remove sidebar collapsible (#74)

--- apps/engine/src/modules/app-layout/app-layout.tsx
+++ apps/engine/src/modules/app-layout/app-layout.tsx
@@ -2,7 +2,6 @@ import {
   Separator,
   SidebarInset,
   SidebarProvider,
-  SidebarTrigger,
 } from '@ds-project/components';
 import { BreadcrumbNav } from './breadcrumb-nav';
 import { AppSidebar } from './app-sidebar';
@@ -20,8 +19,8 @@ export function AppLayout({
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
+            {/* <SidebarTrigger className="-ml-1" /> */}
+            {/* <Separator orientation="vertical" className="mr-2 h-4" /> */}
             <BreadcrumbNav />
           </div>
         </header>
